678ff31d397d7c985ae9b38b1207aeeb02bda105
BrunoXIII-Nova <150086780+BrunoXIII-Nova@users.noreply.github.com>
2023-11-20 23:28:48 -0500

    Update Proyecto_simulacion.py

---
+++ Proyecto_simulacion.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Diccionario de archivos de Excel y sus respectivos nombres de columnas
+archivos_excel = {
+    "Libro1.xlsx": ['Frecuencia con la que lees', 'de 1 a 7'],
+    "Libro2.xlsx": ['¿Sabes leer y escribir en castellano?', 'de 1 a 2'],
+    "Libro3.xlsx": ['¿Con qué frecuencia Realizaron la actividad de: Contar relatos, cuentos, historias?', 'de 1 a 3'],
+    "Libro4.xlsx": ['Número de computadoras/laptops en el hogar', 'de 1 a 10']
+}
+
+# Crear un cuadro combinado para seleccionar el archivo de Excel
+archivo_seleccionado = st.selectbox("Selecciona un archivo de Excel", list(archivos_excel.keys()))
+
+# Leer el archivo de Excel seleccionado
+df = pd.read_excel(f"C:/Users/JOSE/Documents/proyecto_simulacion_PA/{archivo_seleccionado}", names=archivos_excel[archivo_seleccionado])
+
+
+st.dataframe(df)
+
+df.plot(kind='bar')
+st.pyplot(plt)
