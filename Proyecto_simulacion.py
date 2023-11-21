2b56a6d91f21f680d85f1bd5e1ea9587d5d807d0
BrunoXIII-Nova <150086780+BrunoXIII-Nova@users.noreply.github.com>
2023-11-20 23:43:29 -0500

    Update Proyecto_simulacion.py

--- Proyecto_simulacion.py
+++ Proyecto_simulacion.py
@@ -6,18 +6,18 @@ import numpy as np
 # Diccionario de archivos de Excel y sus respectivos nombres de columnas
 archivos_excel = {
     "Libro1.xlsx": ['Frecuencia con la que lees', 'de 1 a 7'],
-    "Libro2.xlsx": ['¿Sabes leer y escribir en castellano?', 'de 1 a 2'],
-    "Libro3.xlsx": ['¿Con qué frecuencia Realizaron la actividad de: Contar relatos, cuentos, historias?', 'de 1 a 3'],
-    "Libro4.xlsx": ['Número de computadoras/laptops en el hogar', 'de 1 a 10']
+    "Libro2.xlsx": ['Columna1', 'Columna2'],
+    "Libro3.xlsx": ['ColumnaA', 'ColumnaB'],
+    "Libro4.xlsx": ['ColumnaX', 'ColumnaY']
 }
 
 # Crear un cuadro combinado para seleccionar el archivo de Excel
 archivo_seleccionado = st.selectbox("Selecciona un archivo de Excel", list(archivos_excel.keys()))
 
 # Leer el archivo de Excel seleccionado
-df = pd.read_excel(f"C:/Users/JOSE/Documents/proyecto_simulacion_PA/{archivo_seleccionado}", names=archivos_excel[archivo_seleccionado])
+df = pd.read_excel(f"{archivo_seleccionado}", names=archivos_excel[archivo_seleccionado])
 
-
+# Mostrar el DataFrame completo en Streamlit
 st.dataframe(df)
 
 df.plot(kind='bar')
